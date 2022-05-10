ce27f9b17fc1aa93d2398f3003ea735ddd784c3e
tyska1 <101899522+tyska1@users.noreply.github.com>
2022-05-10 23:18:23 +0200

    dodanie różnych kolorów kulek

--- Java_Game/src/project/Balls.java
+++ Java_Game/src/project/Balls.java
@@ -2,29 +2,33 @@ package project;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Balls extends Figure {
+
+	Color[] colors = {Color.CYAN, Color.GREEN, Color.MAGENTA, Color.ORANGE, Color.RED, Color.PINK};
+	Color color;
 
 	public Balls(int X, int Y, int punkty) {
 		super(X, Y);
 		this.delay = 1000;
 		this.punkty = punkty;
+		this.color = colors[new Random().nextInt(colors.length)];
+		System.out.println((X-10) + "   " + (Y-10));
 	}
-	
+
 	@Override
 	void draw(Graphics g) {
-		//g.setColor(new Color(255 - 5*punkty, 5*punkty, 0));
-		g.setColor(Color.pink);
+		g.setColor(color);
 		g.fillOval(X-10, Y-10, 20, 20);
-		////g.fillOval(X, Y, 20, 20);
 		g.setColor(Color.white);
 		g.drawString(punkty + "", X-5, Y+5);
+
 		
 	}
 
 	@Override
 	void changeState() {
-		// TODO Auto-generated method stub
 		if (punkty > 0) {
 			punkty--;}
 	}
