4d0c3329c5a19940d531adb8067b3f2e58929f35
tyska1 <101899522+tyska1@users.noreply.github.com>
2022-05-10 23:05:39 +0200

    Dodanie licznika czasu i punktów, zmiana kolorów

--- Java_Game/src/project/Game.java
+++ Java_Game/src/project/Game.java
@@ -1,50 +1,66 @@
 package project;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.Timer;
+import javax.swing.*;
 import javax.swing.plaf.metal.MetalComboBoxButton;
 
 public class Game extends JFrame implements KeyListener, ActionListener{
 
+	Color startPacman = Color.blue, startBoard;
 	Board board = new Board();
-	Pacman pacman = new Pacman(30,40);
+	Pacman pacman = new Pacman(30,40, startPacman);
 	Timer t = new Timer(10,this);
 	Balls[] balls = new Balls[10];
 	Random r = new Random();
 	int par;
+	JLabel jlabel = new JLabel("Press ENTER to start the game!", SwingConstants.CENTER);
+	JLabel timelabel = new JLabel();
+	JLabel pointsLabel = new JLabel();
+	Timer timeCounter;
+	JPanel result = new JPanel();
+	JPanel menuPanel = new JPanel();
+	JButton pacmanColor, boardColor;
+	int time;
 	
 	Game(int width, int height, String name, int known) {
 		this.setResizable(false);
-		this.setSize(500, 700);
+		this.setSize(920, 900);
+		pacmanColor = new JButton("Pacman color");
+		pacmanColor.addActionListener(this);
+		pacmanColor.setFocusable(false);
+		boardColor = new JButton("Board color");
+		boardColor.addActionListener(this);
+		boardColor.setFocusable(false);
+		menuPanel.add(pacmanColor);
+		menuPanel.add(jlabel);
+		menuPanel.add(timelabel);
+		menuPanel.add(pointsLabel);
+		menuPanel.add(boardColor);
+		timelabel.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+		timelabel.setBounds(0,0,100,100);
+		timelabel.setText("TIME: \n  " + time + "  ");
+		timelabel.setVisible(false);
+
+		pointsLabel.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+		pointsLabel.setBounds(0,0,100,100);
+		pointsLabel.setText("POINTS: \n  " + pacman.punkty + "  ");
+		pointsLabel.setVisible(false);
+		menuPanel.setBounds(0, 0, 900, 100);
+		board.setBounds(50, 100, 800, 700);
+
 		this.add(board);
-		board.setBounds(500, 700, 0, 0);
+		this.add(menuPanel);
+		//board.setBounds(500, 700, 0, 0);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		//this.setLocation(width/4, height/4);		
+		//this.setLocation(width/4, height/4);
 		this.setTitle(name);
 		//frame1.setIconImage(Toolkit.getDefaultToolkit().getImage("pacman.png"));
 		this.setVisible(true);
@@ -52,18 +68,24 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 		addKeyListener(this);
 		t.start();
 		this.par = known;
+		jlabel.setFont(new Font("Verdana",1,20));
+		jlabel.setForeground(Color.RED);
+		timelabel.setFont(new Font("Verdana",1,30));
+		pointsLabel.setFont(new Font("Verdana",1,30));
 		
 	}
 	
 	public class Board extends JPanel {
 		
 		boolean isOver = false;
+		Color color;
 		
 		Board(){}
 
 		protected void paintComponent(Graphics g) {
-			g.setColor(Color.darkGray);
-			g.fillRect(0, 0, getWidth(), getHeight()); 
+			g.setColor(color);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
 			pacman.draw(g);
 			for (int i = 0; i < 10; i++) {
 				if (balls[i] == null) {
@@ -73,6 +95,7 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 				if (pacman.eat(balls[i])) {
 					pacman.punkty += balls[i].punkty;
 					balls[i] = null;
+					pointsLabel.setText("POINTS: \n  " + pacman.punkty + "  ");
 					if(this.isAllBallsNull()) {
 						isOver = true;
 					}
@@ -86,7 +109,9 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 			
 			
 		}
-		
+
+
+
 		public void showResult() {
 			
 		}
@@ -120,6 +145,7 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 		        {
 		            pointsArray[(int)(Math.random() * size)]++;
 		        }
+		        int suma = Arrays.stream(pointsArray).sum();
 		        return pointsArray;
 		    }
 		
@@ -128,12 +154,14 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 			int x,y, randX,randY,punkty;
 	//		List<Integer> sumPoints = createPointsList();
 			int pointsTable[] = createPointsArray(10,300);
+			int suma = Arrays.stream(pointsTable).sum();
 	//		System.out.println(sumPoints.toString());
 			for (int i = 0; i < 10; i++) {
 			randX = r.nextInt(getWidth()-20);
 			x = randX < 10 ? randX + 10 : randX;
 			randY = r.nextInt(getHeight()-20);
 			y = randY < 10 ? randY + 10 : randY;
+
 			if (par == 0){
 	//			punkty = pointsArray[i];
 				punkty = pointsTable[i];
@@ -142,6 +170,16 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 			}
 			balls[i] = new Balls(x,y,punkty);
 			}}
+	}
+
+	public void simpleTimer() {
+		timeCounter = new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				time++;
+				timelabel.setText("TIME: \n  " + time + "  ");
+			}
+		});
 	}
 
 	@Override
@@ -155,10 +193,10 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 		// TODO Auto-generated method stub
 			int keyCode = e.getKeyCode();
 			if (keyCode == KeyEvent.VK_UP) {
-			pacman.direction(0, -1); // w górê
+			pacman.direction(0, -1); // w gï¿½rï¿½
 			}
 			if (keyCode == KeyEvent.VK_DOWN) {
-			pacman.direction(0, 1); // w dó³
+			pacman.direction(0, 1); // w dï¿½
 			}
 			if (keyCode == KeyEvent.VK_LEFT) {
 			pacman.direction(-1, 0); // w lewo
@@ -182,28 +220,56 @@ public class Game extends JFrame implements KeyListener, ActionListener{
 	}
 	
 	private void start() {
-		pacman = new Pacman(30, 40);
+		result.setVisible(false);
+		pacman = new Pacman(30, 40, startPacman);
 		pacman.direction(1, 0);
 		new Thread(pacman).start();
 		board.setPosition();
 		for (int i = 0; i < 10; i++) {
 		new Thread(balls[i]).start();
 		}
+		time = 0;
+		this.simpleTimer();
+		jlabel.setVisible(false);
+		timelabel.setVisible(true);
+		pointsLabel.setVisible(true);
+		timeCounter.restart();
+
 		}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+
+		if(e.getSource() == pacmanColor)
+		{
+			Color newColor = JColorChooser.showDialog(null, "Choose color", Color.white);
+			startPacman = newColor;
+			pacman.setColor(newColor);
+		}
+		if(e.getSource() == boardColor)
+		{
+			Color newColor = JColorChooser.showDialog(null, "Choose color", Color.white);
+			startBoard = newColor;
+			board.color = newColor;
+		}
+
 		board.repaint() ;
 
 		if(board.isOver) {//System.out.println(pacman.punkty);
-			this.removeAll();
+			//this.removeAll();
 			//Board result = new Board();
 			//result.setBounds(500, 700, 0, 0);
 			//this.add(result);
 			//result.setVisible(true);
-			this.dispose();
-			board.isOver = false;
+			//this.dispose();
+			//board.isOver = false;
+			timeCounter.stop();
+			board.setVisible(false);
+			menuPanel.setVisible(false);
+			this.add(result);
+			result.setVisible(true);
+
+			result.add(new JLabel("KONIEC, czas: "+time+" punkty: " + pacman.punkty));
 		}
 		
 		//board.repaint() ;
