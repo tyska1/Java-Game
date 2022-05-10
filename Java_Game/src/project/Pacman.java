44dad2647f4d4af2aa21edaf2b90254044fbc899
tyska1 <101899522+tyska1@users.noreply.github.com>
2022-05-10 23:16:14 +0200

    dodanie do konstruktora koloru

--- Java_Game/src/project/Pacman.java
+++ Java_Game/src/project/Pacman.java
@@ -2,15 +2,18 @@ package project;
 
 import java.awt.Color;
 import java.awt.Graphics;
+
 public class Pacman extends Figure {
 
 	private int dx, dy;
+	private Color color;
 	boolean eat(Balls p) {
 		return Math.sqrt((X - p.X)*(X - p.X) + (Y - p.Y) * (Y - p.Y)) < 15;
 		}
 	@Override
 	void draw(Graphics g) {
-		g.setColor(Color.yellow);
+		// TODO Auto-generated method stub
+		g.setColor(this.color);
 		int alfa = 45;
 		if (dx < 0) alfa = 180 + 45;
 		if (dx > 0) alfa = 45;
@@ -24,25 +27,36 @@ public class Pacman extends Figure {
 
 	@Override
 	void changeState() {
+		// TODO Auto-generated method stub
 		X += dx;
 		Y += dy;
 		
 		if (X < 15) { X = 15; dx = -dx;}
-		if (X > 470) { X = 470; dx = -dx; }
+		if (X > 785) { X = 785; dx = -dx; }
 		
-		if (Y < 10) { Y = 10; dy = -dy;}
-		if (Y > 630) { Y = 630; dy = -dy; }
+		if (Y < 15) { Y = 15; dy = -dy;}
+		if (Y > 685) { Y = 685; dy = -dy; }
 	}
 	
-	Pacman(int X, int Y) {
+	Pacman(int X, int Y, Color color) {
 		super(X, Y);
 		dx = 0;
 		dy = 0;
 		delay = 7;
+		this.color = color;
 		}
 	
 	public void direction(int dx, int dy) {
 		this.dx = dx;
 		this.dy = dy;
 		}
+
+
+		public void setColor(Color color) {
+		this.color = color;
+		}
+	
+	
+
+	
 }
